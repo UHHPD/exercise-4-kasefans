b9a15d5591503b0cfb80a2159a8286750ed39d88
SchroederRoberta <139751009+SchroederRoberta@users.noreply.github.com>
2024-12-07 21:53:44 +0100

    Update Data.cc
    Exercise 1

--- Data.cc
+++ Data.cc
@@ -31,11 +31,51 @@ Data::Data(const std::string& filename) {
     file >> entries;
     m_data.push_back(entries);
   }
+  // read in uncertainties from file: m_siz errors
+  for(int i = 0; i < size; ++i){
+    double uncertainties;
+    file >> uncertainties;
+    m_errors.push_back(uncertainties);
+  }
 
   // done! close the file
   file.close();
 
   assertSizes();
+
 };
 
 void Data::assertSizes() { assert(m_data.size() + 1 == m_bins.size()); }
+
+bool Data::check_compatibility_single(Data d1, int bin, int n) {
+  double error = error_prop(d1.error(bin), this->error(bin));
+  return std::abs (d1.measurement(bin) - this->measurement(bin)) < n * error;
+}
+
+int Data::checkCompatibility(const Data& in, int n) {
+  int comp = 0;
+  int notcomp = 0;
+  for(int i = 0; i < in.size(); i++) {
+    if (this->check_compatibility_single(in, i, n)) {
+      comp++;
+    }
+    else {
+      notcomp++;
+    }
+  }
+  return notcomp;
+}
+
+Data Data::operator+(Data& in){
+  Data out;
+  for (int i = 0; i < in.size(); i++) {
+    double w1 = 1 / (this->error(i) * this->error(i));
+    double w2 = 1 / (in.error(i) * in.error(i));
+    double y1 = this->measurement(i);
+    double y2 = in.measurement(i);
+    
+    out.m_data.push_back((w1 * y1 + w2 * y2) / (w1 + w2));
+    out.m_errors.push_back(std::sqrt(1 / (w1 + w2)));
+  }
+  return out;
+}
