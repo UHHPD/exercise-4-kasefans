dc447a6d4896a9d5dd1b5d2cd56958d0bcd97adc
SchroederRoberta <139751009+SchroederRoberta@users.noreply.github.com>
2024-12-07 21:55:22 +0100

    Update run.cc

--- run.cc
+++ run.cc
@@ -66,14 +66,51 @@ int main() {
   cout << "******************************************************" << endl;
   runTests();
   cout << "******************************************************" << endl;
-  // create an object which holds data of experiment A
+  // create objects which hold data of experiments A, B, C, and D
   Data datA("exp_A");
+  Data datB("exp_B");
+  Data datC("exp_C");
+  Data datD("exp_D");
 
   // here is the data from experiment A
   cout << "bin 27: from " << datA.binLow(27) << " to " << datA.binHigh(27)
        << endl;
   cout << "measurement of experiment A in bin 27: " << datA.measurement(27)
        << endl;
+  cout << "******************************************************" << endl;
+  // here is the data from experiment B
+  cout << "bin 27: from " << datB.binLow(27) << " to " << datB.binHigh(27)
+       << endl;
+  cout << "measurement of experiment B in bin 27: " << datB.measurement(27)
+       << endl;
+  cout << "******************************************************" << endl;
+  // here is the data from experiment C
+  cout << "bin 27: from " << datC.binLow(27) << " to " << datC.binHigh(27)
+       << endl;
+  cout << "measurement of experiment C in bin 27: " << datC.measurement(27)
+       << endl;
+  cout << "******************************************************" << endl;
+  // here is the data from experiment D
+  cout << "bin 27: from " << datD.binLow(27) << " to " << datD.binHigh(27)
+       << endl;
+  cout << "measurement of experiment D in bin 27: " << datD.measurement(27)
+       << endl;
+  cout << "******************************************************" << endl;
+
+  // Check compatibility for experiment A and B in bin 27
+  cout << "check_compatibility_single(A,B,27,1): " << datA.check_compatibility_single(datB, 27, 1) << endl;
+  cout << "******************************************************" << endl;
+  
+  // Check compatibility for experiment A and B for 1 to 5 standard deviations
+  cout << "checkCompatibility(A, B, 1) = " << datA.checkCompatibility(datB, 1) << endl;
+  cout << "checkCompatibility(A, B, 2) = " << datA.checkCompatibility(datB, 2) << endl;
+  cout << "checkCompatibility(A, B, 3) = " << datA.checkCompatibility(datB, 3) << endl;
+  cout << "checkCompatibility(A, B, 4) = " << datA.checkCompatibility(datB, 4) << endl;
+  cout << "checkCompatibility(A, B, 5) = " << datA.checkCompatibility(datB, 5) << endl;
+  cout << "******************************************************" << endl;
+
+  // Average datasets 
+  cout << "******************************************************" << endl;
 
   return 0;
 }
