bab7c69a91c3d517ed855c15a6a337fa1493d67e
maschahackmann <150017453+maschahackmann@users.noreply.github.com>
2024-12-08 13:42:53 +0000

    Remove seperators *

--- run.cc
+++ run.cc
@@ -64,9 +64,8 @@ void runTests() {
 int main() {
   using namespace std;
 
-  cout << "******************************************************" << endl;
   runTests();
-  cout << "******************************************************" << endl;
+
   // create objects which hold data of experiments A, B, C, and D
   Data datA("exp_A");
   Data datB("exp_B");
@@ -82,49 +81,43 @@ int main() {
        << endl;
   cout << "measurement of experiment A in bin 27: " << datA.measurement(27)
        << endl;
-  cout << "******************************************************" << endl;
+  
   // here is the data from experiment B
   cout << "bin 27: from " << datB.binLow(27) << " to " << datB.binHigh(27)
        << endl;
   cout << "measurement of experiment B in bin 27: " << datB.measurement(27)
        << endl;
-  cout << "******************************************************" << endl;
+  
   // here is the data from experiment C
   cout << "bin 27: from " << datC.binLow(27) << " to " << datC.binHigh(27)
        << endl;
   cout << "measurement of experiment C in bin 27: " << datC.measurement(27)
        << endl;
-  cout << "******************************************************" << endl;
+  
   // here is the data from experiment D
   cout << "bin 27: from " << datD.binLow(27) << " to " << datD.binHigh(27)
        << endl;
   cout << "measurement of experiment D in bin 27: " << datD.measurement(27)
        << endl;
-  cout << "******************************************************" << endl;
+  
 
   // Check compatibility for experiment A and B in bin 27
   cout << "check_compatibility_single(A,B,27,1): " << datA.check_compatibility_single(datB, 27, 1) << endl;
-  cout << "******************************************************" << endl;
 
   // Check compatibility for experiment A and C in bin 27
   cout << "check_compatibility_single(A,C,27,1): " << datA.check_compatibility_single(datC, 27, 1) << endl;
-  cout << "******************************************************" << endl;
 
   // Check compatibility for experiment A and D in bin 27
   cout << "check_compatibility_single(A,D,27,1): " << datA.check_compatibility_single(datD, 27, 1) << endl;
-  cout << "******************************************************" << endl;
 
   // Check compatibility for experiment B and C in bin 27
   cout << "check_compatibility_single(B,C,27,1): " << datB.check_compatibility_single(datC, 27, 1) << endl;
-  cout << "******************************************************" << endl;
 
   // Check compatibility for experiment B and D in bin 27
   cout << "check_compatibility_single(B,D,27,1): " << datB.check_compatibility_single(datD, 27, 1) << endl;
-  cout << "******************************************************" << endl;
-
+  
   // Check compatibility for experiment C and D in bin 27
   cout << "check_compatibility_single(C,D,27,1): " << datC.check_compatibility_single(datD, 27, 1) << endl;
-  cout << "******************************************************" << endl;
   
   // Check compatibility for experiment A and B for 1 to 5 standard deviations
   cout << "checkCompatibility(A, B, 1) = " << datA.checkCompatibility(datB, 1) << endl;
@@ -132,7 +125,6 @@ int main() {
   cout << "checkCompatibility(A, B, 3) = " << datA.checkCompatibility(datB, 3) << endl;
   cout << "checkCompatibility(A, B, 4) = " << datA.checkCompatibility(datB, 4) << endl;
   cout << "checkCompatibility(A, B, 5) = " << datA.checkCompatibility(datB, 5) << endl;
-  cout << "******************************************************" << endl;
 
 // Check compatibility for dataset datA
 for (int sig : {2, 3}){
